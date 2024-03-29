6afe95612518297e48938a5e4efe530a1314559a
codec404 <saptarshimemari072@gmail.com>
2024-03-29 13:31:31 +0530

    Change in fetch problems

--- src/components/AdminProblem.jsx
+++ src/components/AdminProblem.jsx
@@ -1,13 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/AdminProblem.css";
-import { useNavigate } from "react-router-dom";
-import ProblemList from "./ProblemList";
-import { Input } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import { Select, Input } from "antd";
+import axios from "axios";
 
-const HomeCard = () => {
+const { Option } = Select;
+
+const AdminProblem = () => {
+  const [problems, setProblems] = useState([]);
+  const fetchProblems = async () => {
+    try {
+      const res = await axios.get("/api/v1/admin/problems");
+      if (res?.data?.success) {
+        setProblems(res?.data?.problems);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProblems();
+  }, [problems]);
+  const [query, setQuery] = useState("");
+  const [diff, setDiff] = useState("");
+  const [tagQuery, setTagQuery] = useState("");
   const navigate = useNavigate();
   const handleAddProblem = () => {
     navigate("/add-new-problem");
+  };
+  const handleEdit = (id) => {
+    navigate(`/edit/problem/${id}`);
+  };
+  const handleGetDifficulty = (value) => {
+    setDiff(value);
   };
   return (
     <>
@@ -25,6 +51,9 @@ const HomeCard = () => {
               name="searchitem"
               placeholder="Search"
               className="search-container"
+              onChange={(e) => {
+                setQuery(e.target.value);
+              }}
             />
             <div className="search-button">
               <button type="submit">
@@ -39,11 +68,24 @@ const HomeCard = () => {
             <div className="filterBody">
               <div className="filterTags filterOptions">
                 <label htmlFor="filterTags">Tags</label>
-                <Input />
+                <Input
+                  onChange={(e) => {
+                    setTagQuery(e.target.value);
+                  }}
+                />
               </div>
               <div className="filterOptions filterDifficulty">
                 <label htmlFor="filterDifficulty">Difficulty</label>
-                <Input />
+                <Select
+                  className="filter-difficulty-options"
+                  value={diff}
+                  onChange={handleGetDifficulty}
+                >
+                  <Option value={""}>-</Option>
+                  <Option value="easy">Easy</Option>
+                  <Option value="medium">Medium</Option>
+                  <Option value="hard">Hard</Option>
+                </Select>
               </div>
             </div>
           </div>
@@ -52,11 +94,64 @@ const HomeCard = () => {
               <span>+</span>Add Problem
             </button>
           </div>
-          <ProblemList />
+          <div className="problem-header">
+            <div className="edit"></div>
+            <div className="problemTitle">Title</div>
+            <div className="problemTags">Tags</div>
+            <div className="problemDifficulty">Difficulty</div>
+          </div>
+          {problems
+            .filter((problem) =>
+              problem.problemName.toLowerCase().includes(query.toLowerCase())
+            )
+            .filter((problem) => (diff ? problem.difficulty === diff : problem))
+            .filter((problem) =>
+              problem.problemTags.some((problemTag) =>
+                problemTag.toLowerCase().includes(tagQuery.toLowerCase())
+              )
+            )
+            .map((item, index) => (
+              <div
+                className={`problem-container ${index & 1 ? "even" : ""}`}
+                key={item._id}
+              >
+                <span className="editBtn">
+                  <button onClick={() => handleEdit(item._id)}>
+                    <i className="fa-solid fa-pen-to-square" />
+                  </button>
+                </span>
+                <span className="problem-name">
+                  <Link to={`/problem/${item._id}`}>{item.problemName}</Link>
+                </span>
+                <div className="problem-tags">
+                  {item.problemTags.map((tag) => (
+                    <span className="problem-tag" key={tag}>
+                      <div className="tags">
+                        <span>{tag}</span>
+                      </div>
+                    </span>
+                  ))}{" "}
+                </div>
+                <span
+                  className={`problem-difficulty ${
+                    item.difficulty === "easy"
+                      ? "easy"
+                      : item.difficulty === "medium"
+                      ? "medium"
+                      : "hard"
+                  }`}
+                >
+                  <span>
+                    {item.difficulty.charAt(0).toUpperCase() +
+                      item.difficulty.substring(1)}
+                  </span>
+                </span>
+              </div>
+            ))}
         </div>
       </div>
     </>
   );
 };
 
-export default HomeCard;
+export default AdminProblem;
--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/HomeCard.css";
-import ProblemList from "./ProblemList";
-import { Input } from "antd";
+import { Input, Select } from "antd";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const { Option } = Select;
 
 const HomeCard = () => {
+  const [problems, setProblems] = useState([]);
+  const fetchProblems = async () => {
+    try {
+      const res = await axios.get("/api/v1/admin/problems");
+      if (res?.data?.success) {
+        setProblems(res?.data?.problems);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProblems();
+  }, [problems]);
+  const [query, setQuery] = useState("");
+  const [diff, setDiff] = useState("");
+  const [tagQuery, setTagQuery] = useState("");
+
+  const handleGetDifficulty = (value) => {
+    setDiff(value);
+  };
   return (
     <>
       <div className="home-main-container">
@@ -20,6 +45,9 @@ const HomeCard = () => {
               name="searchitem"
               placeholder="Search"
               className="search-container"
+              onChange={(e) => {
+                setQuery(e.target.value);
+              }}
             />
             <div className="search-button">
               <button type="submit">
@@ -34,16 +62,82 @@ const HomeCard = () => {
             <div className="filterBody">
               <div className="filterTags filterOptions">
                 <label htmlFor="filterTags">Tags</label>
-                <Input />
+                <Input
+                  onChange={(e) => {
+                    setTagQuery(e.target.value);
+                  }}
+                />
               </div>
               <div className="filterOptions filterDifficulty">
                 <label htmlFor="filterDifficulty">Difficulty</label>
-                <Input />
+                <Select
+                  className="filter-difficulty-options"
+                  value={diff}
+                  onChange={handleGetDifficulty}
+                >
+                  <Option value={""}>-</Option>
+                  <Option value="easy">Easy</Option>
+                  <Option value="medium">Medium</Option>
+                  <Option value="hard">Hard</Option>
+                </Select>
               </div>
             </div>
           </div>
         </div>
-        <ProblemList />
+        <div className="problem-header user-problem-header">
+          <div className="edit mark"></div>
+          <div className="problemTitle">Title</div>
+          <div className="problemTags">Tags</div>
+          <div className="problemDifficulty">Difficulty</div>
+        </div>
+        {problems
+          .filter((problem) =>
+            problem.problemName.toLowerCase().includes(query.toLowerCase())
+          )
+          .filter((problem) => (diff ? problem.difficulty === diff : problem))
+          .filter((problem) =>
+            problem.problemTags.some((problemTag) =>
+              problemTag.toLowerCase().includes(tagQuery.toLowerCase())
+            )
+          )
+          .map((item, index) => (
+            <div
+              className={`problem-container user-problem-container ${
+                index & 1 ? "even" : ""
+              }`}
+              key={item._id}
+            >
+              <span className="editBtn markBtn">
+                {/* <span>{index + 1}</span> */}
+              </span>
+              <span className="problem-name">
+                <Link to={`/problem/${item._id}`}>{item.problemName}</Link>
+              </span>
+              <div className="problem-tags">
+                {item.problemTags.map((tag) => (
+                  <span className="problem-tag" key={tag}>
+                    <div className="tags">
+                      <span>{tag}</span>
+                    </div>
+                  </span>
+                ))}{" "}
+              </div>
+              <span
+                className={`problem-difficulty ${
+                  item.difficulty === "easy"
+                    ? "easy"
+                    : item.difficulty === "medium"
+                    ? "medium"
+                    : "hard"
+                }`}
+              >
+                <span>
+                  {item.difficulty.charAt(0).toUpperCase() +
+                    item.difficulty.substring(1)}
+                </span>
+              </span>
+            </div>
+          ))}
       </div>
     </>
   );
